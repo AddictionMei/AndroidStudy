bace00ba5fc9c2a64c5465a0ab2652bc6af4f48f
AddictionMei <abandon01@163.com>
2017-09-11 18:49:49 +0800

    9月11日周一第一次

--- criminalintent/src/main/java/com/example/criminalintent/CrimeFragment.java
+++ criminalintent/src/main/java/com/example/criminalintent/CrimeFragment.java
@@ -9,6 +9,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 
 /**
@@ -19,6 +21,7 @@ public class CrimeFragment extends Fragment {
     private Crime mCrime;
     private EditText mTitleField;
     private Button mDateButton;
+    private CheckBox mSolvedCheckBox;
 
 
     @Override
@@ -58,6 +61,15 @@ public class CrimeFragment extends Fragment {
         mDateButton=(Button)v.findViewById(R.id.crime_date);
         mDateButton.setText(mCrime.getDate().toString());
         mDateButton.setEnabled(false);
+
+        mSolvedCheckBox=(CheckBox)v.findViewById(R.id.crime_solved);
+        mSolvedCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                mCrime.setSolved(isChecked);
+
+            }
+        });
         return v;
     }
 }
