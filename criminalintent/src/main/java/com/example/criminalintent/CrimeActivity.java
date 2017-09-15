5443daee401852eb6b81be195a78fbdc1bcd1f3b
AddictionMei <abandon01@163.com>
2017-09-15 09:53:49 +0800

    9月15日周五第二次

--- criminalintent/src/main/java/com/example/criminalintent/CrimeActivity.java
+++ criminalintent/src/main/java/com/example/criminalintent/CrimeActivity.java
@@ -1,13 +1,31 @@
 package com.example.criminalintent;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
 
 public class CrimeActivity extends AppCompatActivity {
+//    Called when the activity first created.
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_crime);
+        setContentView(R.layout.activity_fragment);
+
+
+        FragmentManager fm=getSupportFragmentManager();
+        Fragment fragment =fm.findFragmentById(R.id.fragment_container);
+
+        if(fragment == null){
+            fragment = new CrimeFragment();
+            //创建提交了一个Fragment事务
+            fm.beginTransaction().add(R.id.fragment_container, fragment).commit();
+
+
+
+        }
+
+
     }
 }
